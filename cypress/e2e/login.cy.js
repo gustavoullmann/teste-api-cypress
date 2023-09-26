9e4c7fcae33fce80a22a255e436553b01b3252b3
Gustavo Ullmann <gustavo.ullmann@cwi.com.br>
2023-09-26 18:06:35 -0300

    Criação do teste da funcionalidade de Login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,18 @@
+/// <reference types = "cypress"/>
+
+describe('Login - Testes da API ServeRest', () => {
+    
+    it('Deve fazer login com sucesso', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/login',
+            body: {
+                "email": "Ralph_McKenzie21@yahoo.com",
+                "password": "teste"
+              }
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body.message).to.equal("Login realizado com sucesso")
+        })
+    });
+});
