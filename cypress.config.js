ac33a68fb43e76c29a93ca622cfc0e7ac4f3b700
Gustavo Ullmann <gustavo.ullmann@cwi.com.br>
2023-09-26 19:27:04 -0300

    Criada baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      
-    },
+    baseUrl: 'http://localhost:3000'
   },
-  watchForFileChanges: false
 });
