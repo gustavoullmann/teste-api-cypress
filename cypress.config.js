b651ff21bd249795d70d720cccf5fc1bb98643fd
Gustavo Ullmann <gustavoullmann@gmail.com>
2023-10-03 20:12:40 -0300

    inclusão do reporter mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,17 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
   watchForFileChanges: false,
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "mochawesome-report",
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl: 'http://localhost:3000'
   },
+
 });
