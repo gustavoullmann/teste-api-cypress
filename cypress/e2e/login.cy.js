3d6745235eaf09e7e14c8b706b33b10ec2bc387d
Gustavo Ullmann <gustavoullmann@gmail.com>
2024-01-08 16:05:17 -0300

    Teste para cadastro de usuário

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,22 @@
 /// <reference types = "cypress"/>
 
 describe("Login - Testes da API ServeRest", () => {
+  it("Cadastra usuário com sucesso", () => {
+    cy.request({
+      method: "POST",
+      url: "/usuarios",
+      body: {
+        nome: "Fulano da Silva",
+        email: "Ralph_McKenzie21@yahoo.com",
+        password: "teste",
+        administrador: "true"
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(201);
+      expect(response.body.message).to.equal("Cadastro realizado com sucesso");
+    });
+  })
+
   it("Deve fazer login com sucesso", () => {
     cy.request({
       method: "POST",
